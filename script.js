74536868d1b48b724fa14cf52deae12de14059de
Felipe Hoffmann <felipehoffmann@MacBook-Pro-de-Felipe.local>
2020-01-13 22:38:28 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,30 @@
+﻿var nomes = ["Diego", "Gabriel", "Lucas"];
+
+var listElement = document.createElement("ul");
+var inputElement = document.getElementById("inputText");
+var body = document.body;
+
+function adicionar() {
+
+    listElement.innerHTML = '';
+
+    var textInput = inputElement.value;
+    nomes.push(textInput);
+    inputElement.value = null;
+
+    adicionarTodos();
+}
+
+function adicionarTodos(){
+    
+    for (nome of nomes) {
+        var itemListElement = document.createElement("li");
+        var textNome = document.createTextNode(nome);
+
+        itemListElement.appendChild(textNome);
+        listElement.appendChild(itemListElement);
+    }
+}
+
+adicionarTodos();
+body.appendChild(listElement);
